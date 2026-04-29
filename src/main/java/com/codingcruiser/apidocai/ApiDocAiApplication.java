4aeac4a0b92cf64fec38da2c0950ad0888913a1a
Prashant Balotra <balotra.prashant@bcstechnology.com.au>
2026-04-29 14:33:44 +0530

    first commit

---
+++ src/main/java/com/codingcruiser/apidocai/ApiDocAiApplication.java
@@ -0,0 +1,13 @@
+package com.codingcruiser.apidocai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDocAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDocAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingcruiser/apidocai/ApiDocAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingcruiser.apidocai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDocAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
